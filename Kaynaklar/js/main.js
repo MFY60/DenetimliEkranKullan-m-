808cf87413414d8984a20d897d24f3a033f50b0e
MFY60 <meliksah_yalcin@icloud.com>
2022-08-22 17:13:11 +0300

    İlk Deneme

--- Kaynaklar/js/main.js
+++ Kaynaklar/js/main.js
@@ -52,15 +52,15 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
+    duration: 1500,
     delay: 400,
 })
 
 sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`, {delay: 500})
-sr.reveal(`.profile__profession`, {delay: 600})
-sr.reveal(`.profile__social`, {delay: 700})
-sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
-sr.reveal(`.profile__buttons`, {delay: 800})
-sr.reveal(`.filters__content`, {delay: 900})
-sr.reveal(`.filters`, {delay: 1000})
+sr.reveal(`.profile__name`, {delay: 100})
+sr.reveal(`.profile__profession`, {delay: 200})
+sr.reveal(`.profile__social`, {delay: 300})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 400})
+sr.reveal(`.profile__buttons`, {delay: 400})
+sr.reveal(`.filters__content`, {delay: 500})
+sr.reveal(`.filters`, {delay: 600})
